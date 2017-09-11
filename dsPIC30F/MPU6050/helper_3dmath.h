a6f28809713f433bff0478efcf8604b00da22a96
Daichou <tommy0705c@gmail.com>
2017-09-11 16:08:03 +0800

    Create Quaternions structure

--- dsPIC30F/MPU6050/helper_3dmath.h
+++ dsPIC30F/MPU6050/helper_3dmath.h
@@ -32,58 +32,64 @@ THE SOFTWARE.
 #ifndef _HELPER_3DMATH_H_
 #define _HELPER_3DMATH_H_
 
-struct Quaternion {
+struct quaternion {
     float w;
     float x;
     float y;
     float z;
-        Quaternion() {
-            w = 1.0f;
-            x = 0.0f;
-            y = 0.0f;
-            z = 0.0f;
-        }
-        Quaternion(float nw, float nx, float ny, float nz) {
-            w = nw;
-            x = nx;
-            y = ny;
-            z = nz;
-        }
-        Quaternion getProduct(Quaternion q) {
-            // Quaternion multiplication is defined by:
-            //     (Q1 * Q2).w = (w1w2 - x1x2 - y1y2 - z1z2)
-            //     (Q1 * Q2).x = (w1x2 + x1w2 + y1z2 - z1y2)
-            //     (Q1 * Q2).y = (w1y2 - x1z2 + y1w2 + z1x2)
-            //     (Q1 * Q2).z = (w1z2 + x1y2 - y1x2 + z1w2
-            return Quaternion(
-                w*q.w - x*q.x - y*q.y - z*q.z,  // new w
-                w*q.x + x*q.w + y*q.z - z*q.y,  // new x
-                w*q.y - x*q.z + y*q.w + z*q.x,  // new y
-                w*q.z + x*q.y - y*q.x + z*q.w); // new z
-        }
-
-        Quaternion getConjugate() {
-            return Quaternion(w, -x, -y, -z);
-        }
-        
-        float getMagnitude() {
-            return sqrt(w*w + x*x + y*y + z*z);
-        }
-        
-        void normalize() {
-            float m = getMagnitude();
-            w /= m;
-            x /= m;
-            y /= m;
-            z /= m;
-        }
-        
-        Quaternion getNormalized() {
-            Quaternion r(w, x, y, z);
-            r.normalize();
-            return r;
-        }
 };
+typedef struct quaternion Quaternion;
+
+void Quaternion_Init(struct Quaternion * A) {
+    A->w = 1.0f;
+    A->x = 0.0f;
+    A->y = 0.0f;
+    A->z = 0.0f;
+}
+void Quaternion_Construct(Quaternion * A ,float nw, float nx, float ny, float nz) {
+    A->w = nw;
+    A->x = nx;
+    A->y = ny;
+    A->z = nz;
+}
+Quaternion Quaternion_getProduct(Quaternion A ,struct Quaternion B) {
+    // Quaternion multiplication is defined by:
+    //     (Q1 * Q2).w = (w1w2 - x1x2 - y1y2 - z1z2)
+    //     (Q1 * Q2).x = (w1x2 + x1w2 + y1z2 - z1y2)
+    //     (Q1 * Q2).y = (w1y2 - x1z2 + y1w2 + z1x2)
+    //     (Q1 * Q2).z = (w1z2 + x1y2 - y1x2 + z1w2
+    Quaternion outcome;
+    Quaternion_Construct(&outcome,
+        A.w*B..w - A.x*B.x - A.y*B.y - A.z*B.z,  // new w
+        A.w*B..x + A.x*B.w + A.y*B.z - A.z*B.y,  // new x
+        A.w*B..y - A.x*B.z + A.y*B.w + A.z*B.x,  // new y
+        A.w*B..z + A.x*B.y - A.y*B.x + A.z*B.w); // new z
+    return outcome;
+}
+
+Quaternion Quaternion_getConjugate(Quaternion * A) {
+    Quaternion outcome;
+    return Quaternion_Construct(&outcome,A->w, - A->x, - A->y, - A->z);
+}
+
+float Quaternion_getMagnitude(Quaternion * A ) {
+    return sqrt(A->w*A->w + A->x*A->x + A->y*A->y + A->z*A->z);
+}
+
+void Quaternion_normalize(Quaternion * A) {
+    float m = Quaternion_getMagnitude(A);
+    A->w /= m;
+    A->x /= m;
+    A->y /= m;
+    A->z /= m;
+}
+
+Quaternion Quaternion_getNormalized(Quaternion * A ) {
+    Quaternion r;
+    Quaternion_Construct(&r,A->w,A->x,A->y,A->z);
+    Quaternion_normalize(&r);
+    return r;
+}
 
 class VectorInt16 {
     public:
