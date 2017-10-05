d49ec49fcd18e171df31e3074521c644ad5fe230
Daichou <tommy0705c@gmail.com>
2017-10-06 00:40:27 +0800

    change dmp example from ardunio sampe

--- dsPIC30F/MPU6050/Examples/MPU6050_DMP.X/main.c
+++ dsPIC30F/MPU6050/Examples/MPU6050_DMP.X/main.c
@@ -1,5 +1,5 @@
 /*
- * File:   LEDMain.c
+ * File:   main.c
  * Author: tommycc
  *
  * Created on March 1, 2017, 3:49 PM
@@ -39,6 +39,16 @@
 #define true 1
 #define false 0
 
+#define M_PI 3.141592653589793238462643
+#define PI 3.1415926535897932384626433832795
+
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_REALACCEL
+//#define OUTPUT_READABLE_WORLDACCEL
+//#define OUTPUT_TEAPOT
+
 #include <xc.h>
 #include<uart.h>
 #include<stdio.h>
@@ -48,12 +58,32 @@
 #include<libpic30.h>
 #include"../../MPU6050.h"
 #include"../../../I2Cdev/I2Cdev.h"
+#include"../../MPU6050_6Axis_MotionApps20.h"
 /* Received data is stored in array Buf  */
 char Buf[80];
 char* Receivedddata = Buf;
 int16_t ax, ay, az, gx, gy, gz;
 volatile unsigned int mpuInterrupt = false;
 uint8_t devStatus;
+uint8_t mpuInitStatus;
+uint16_t packetSize;
+uint16_t fifocount;
+uint8_t fifoBuffer[64];
+uint8_t dmpReady = false;
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+
 /* This is UART1 transmit ISR */
 void __attribute__((__interrupt__)) _U2TXInterrupt(void)
 {  
@@ -136,8 +166,8 @@ void Init()
     ConfigIntI2C(MI2C_INT_OFF & SI2C_INT_OFF);
     OpenI2C(I2C_config1,I2C_config2);
     OpenUART2(U2MODEvalue, U2STAvalue, baudvalue);
-    
     ConfigINT0(RISING_EDGE_INT & INT_ENABLE & GLOBAL_INT_ENABLE & EXT_INT_PRI_1);
+
     MPU6050(MPU6050_ADDRESS_AD0_LOW);
     printf(Buf,"Initialize MPU6050\n\0");
     MPU6050_initialize();
@@ -145,51 +175,50 @@ void Init()
     unsigned char MPU6050_ID = MPU6050_getDeviceID();
 
     printf("Testing device connections...\n\0");
-    
     printf("MPU6050_ID = 0x%X\n",MPU6050_ID);
-    
     printf(MPU6050_testConnection() ? "MPU6050 connection successful\r\n" :
         "MPU6050 connection failed\r\n");
-    
+
     if (!MPU6050_testConnection())
         continue;
 
     printf("Reading offset\n");
-    
     printf("Xaccel= %d\n",MPU6050_getXAccelOffset());
-    
     printf("Yaccel= %d\n",MPU6050_getYAccelOffset());
-    
     printf("Zaccel= %d\n",MPU6050_getZAccelOffset());
-    
     printf("Xgyro= %d\n",MPU6050_getXGyroOffset());
-    
     printf("Ygyro= %d\n",MPU6050_getYGyroOffset());
-    
     printf("Zgyro= %d\n",MPU6050_getZGyroOffset());
-    
     printf("Initialize DMP.....\n");
-    
-    MPU6050_dmpInitialize();
+
+    devStatus = MPU6050_dmpInitialize();
+
+    if (devStatus == 0) {
+        printf("DMP Enabled.....\n");
+        MPU6050_setDMPEnabled(true);
+        mpuInitStatus = MPU6050_getInitStatus();
+        
+        printf("DMP ready,wait for first interupt\n");
+        dmpReady = true;
+
+        packetSize = MPU6050_dmpGetFIFOPacketSize();
+    } else {
+        printf("DMP Initialize failed!!!\n");
+        printf("devStatus = %u\n",devStatus);
+        printf("\n");
+    } 
 
     MPU6050_setXGyroOffset(220);
     MPU6050_setYGyroOffset(76);
     MPU6050_setZGyroOffset(-85);
 
     printf("Reading Updated offset\n");
-    
     printf("Xaccel= %d\n",MPU6050_getXAccelOffset());
-    
     printf("Yaccel= %d\n",MPU6050_getYAccelOffset());
-    
     printf("Zaccel= %d\n",MPU6050_getZAccelOffset());
-    
     printf("Xgyro= %d\n",MPU6050_getXGyroOffset());
-    
     printf("Ygyro= %d\n",MPU6050_getYGyroOffset());
-    
     printf("Zgyro= %d\n",MPU6050_getZGyroOffset());
-    
     MPU6050_setSleepEnabled(false);
 }
 
@@ -197,20 +226,78 @@ void Init()
 int main(void)
 {
     __C30_UART=2;
+    Init();
+    if (!dmpReady) {
+        while(1);
+    }
     while(1){
-
-        while (true) {
-           // Read raw accel/gyro measurements from device
-            MPU6050_getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-            // Display tab-separated accel/gyro x/y/z values
-            printf("a/g:\t%d\t%d\t%d\t%d\t%d\t%d\r\n", ax, ay, az, gx, gy, gz);
-            // Blink LED to indicate activity
-            LATCbits.LATC14 ^= 1;
-
-            delay_ms(25);
-            delay_ms(25);
+        while(!mpuInterrupt && fifocount < packetSize){
+        
         }
+        mpuInterrupt = false;
+        mpuInitStatus = MPU6050_getInitStatus();
+
+        fifocount = MPU6050_GetFIFOCount();
+        if ( (mpuInitStatus & 0x10) || (fifocount == 1024) ){
+            MPU6050_resetFIFO();
+            printf("FIFO overflow!!");
+        } else if ( mpuInitStatus && 0x02 )
+        {
+            while ( fifocount < packetSize ) fifocount = MPU6050_GetFIFOCount();
+
+            MPU6050_getFIFOBytes(fifoBuffer,packetSize);
+#ifdef OUTPUT_READABLE_QUATERNION
+            MPU6050_dmpGetQuaternion(&q, fifoBuffer);
+            printf("quat\t%f\t%f\t%f\t%f\n",q.w,q.x,q.y,q.z);
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
+            MPU6050_dmpGetQuaternion(&q, fifoBuffer);
+            MPU6050_dmpGetEuler(euler,&q);
+            printf("Euler\t%f\t%f\t%f\n",euler[0] * 180/M_PI,euler[1] * 180/M_PI,euler[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+            MPU6050_dmpGetQuaternion(&q, fifoBuffer);
+            MPU6050_dmpGetGravity(&gravity,&q);
+            MPU6050_dmpGetYawPitchRoll(ypr,&q,&gravity);
+            printf("ypr\t%f\t%f\t%f\n",ypr[0] * 180/M_PI,ypr[1] * 180/M_PI,ypr[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+            MPU6050_dmpGetQuaternion(&q, fifoBuffer);
+            MPU6050_dmpGetAccel(&aa,fifoBuffer);
+            MPU6050_dmpGetGravity(&gravity,&q);
+            MPU6050_dmpGetLinearAccel(&aaReal,&aa,&gravity);
+            printf("areal\t%d\t%d\t%d\n",aaReal.x,aaReal.y,aaReal.z); 
+#endif
+
+#ifdef OUTPUT_READABLE_WORDACCEL
+            MPU6050_dmpGetQuaternion(&q, fifoBuffer);
+            MPU6050_dmpGetAccel(&aa,fifoBuffer);
+            MPU6050_dmpGetGravity(&gravity,&q);
+            MPU6050_dmpGetLinearAccel(&aaReal,&aa,&gravity);
+            MPU6050_dmpGetLinearAccelInWorld(&aaWorld,&aaReal,&q);
+            printf("aworld\t%d\t%d\t%d\n",aaWorld.x,aaWorld.y,aaWorld.z);
+#endif
+
+#ifdef OUTPUT_TEAPOT
+            teapotPacket[2] = fifoBuffer[0];
+            teapotPacket[3] = fifoBuffer[1];
+            teapotPacket[4] = fifoBuffer[4];
+            teapotPacket[5] = fifoBuffer[5];
+            teapotPacket[6] = fifoBuffer[8];
+            teapotPacket[7] = fifoBuffer[9];
+            teapotPacket[8] = fifoBuffer[12];
+            teapotPacket[9] = fifoBuffer[13];
+            putsUART2 ((unsigned int *)fifoBuffer);
+            while(BusyUART2());
+            teapotPacket[11]++;
+#endif
+        }
+        // Blink LED to indicate activity
+        LATCbits.LATC14 ^= 1;
+
         //CloseUART2();
 
     }
